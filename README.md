083dfd57e72bfe8202fc5a2e334b3fb7af1426ac
Rex A <rexcanuforo@yahoo.com>
2020-04-25 23:29:56 -0400

    Finished more parts of the code

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# 07_Homework
-make a read me 
+
+# i
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+## Description
+  i
+ ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+  
+## Installation
+To install necessary dependencies, run the following command:
+    i
+## Usage
+i
+## License
+This project is licensed under the i license.
+## Contributing
+i
+## Tests
+To run tests, run the following command:
+    i
+## Questions
+<img src="https://avatars2.githubusercontent.com/u/2905137?v=4" alt="avatar" style="border-radius: 16px" width="30"/>
+If you have any questions about the repo, open an issue or contact@
+[i]https://github.com/i
+  
--- index.js
+++ index.js
@@ -56,26 +56,38 @@ inquirer.prompt([
 
 ];
 
+var apiName = (data) => {
+  var queryUrl = `https://api.github.com/users/${data.username}`;
+  return axios.get(queryUrl)
+}
+
 // -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
 function writeToFile(fileName, data) {
+    
 }
 
 // -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
-function init() {
 
-}
+   function init() {
+    console.log("Creating the README...")
+    try {
+       
+      const data = await listQuestions();
+  
+      const repo =  await apiName(data);
+  
+      const markdown =  createMarkdown(data,repo.data.avatar_url,repo.data.html_url);
+  
+      await writeFileAsync("README.md", markdown);
+  
+      console.log("Successfully wrote to README.md");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
 
 // -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
 init();
 
 
-
-//   .then(function(response) {
-
-//     if (response.confirm === response.password) {
-//       console.log("Success!");
-//     }
-//     else {
-//       console.log("You forgot your password already?!");
-//     }
-//   });
