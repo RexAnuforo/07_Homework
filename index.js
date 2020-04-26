d0080f3dffa36e086db77474e40d4c9a3852020d
Rex A <rexcanuforo@yahoo.com>
2020-04-25 23:41:00 -0400

    added readme section

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-const fs = require('fs');
-const axios = require("axios");
 const inquirer = require('inquirer');
+const axios = require("axios");
 const util = require('util');
+const fs = require('fs');
 
 // -- They give us an ARRAY called 'questions' What could we do with this (?) -- //
-const questions = [
-    
-inquirer.prompt([
-{
-    type: "input",
-    message: "What is your github user name?",
-    name: "username"
-  },
-  {
-    type: "input",
-    message: "What is your github email?",
-    name: "email"
-  },
-  {
-    type: "input",
-    message: "what is the name of your project",
-    name: "projectName"
-  },
-  {
-      type:"input",
-      message:"Please write a short desrciption of you project",
-      name:"description"
-  },
-  {
-    type: "input",
-    message: "What command should installed",
-    name: "installation"
-  },
-  {
-      type: "input",
-      message: "What kind of license the project should have?",
-      name: "license"
-  },,
-  {
-      type: "input",
-      message: "What command should be run to run tests?",
-      name: "tests"
-  },
-  {
-      type: "input",
-      message: "What does the user need to know about using the repo?",
-      name: "usage"
-  },
-  {
-      type: "input",
-      message: "What does the user need to know about contributing to the repo?",
-      name: "contributing"
-  },
-])
-
-];
+function listQuestions(){
+  return inquirer.prompt ([
+      {
+          type: "input",
+          message: "What is your Github user name?",
+          name: "username"
+      },
+      {
+          type: "input",
+          message: "what is the name of your project?",
+          name: "projectName"
+      },
+      {
+          type:"input",
+          message:"Write a short desrciption of your project",
+          name:"description"
+      },
+      {
+          type: "input",
+          message: "What kind of license will the project have?",
+          name: "license"
+      },
+      {
+          type: "input",
+          message: "What command should be ran for installs?",
+          name: "installation"
+      },
+      {
+          type: "input",
+          message: "What command should be ran to run any tests?",
+          name: "tests"
+      },
+      {
+          type: "input",
+          message: "What does the user need to know about using the repo?",
+          name: "usage"
+      },
+      {
+          type: "input",
+          message: "What does the user need to know about contributing to the repo?",
+          name: "contributing"
+      },
+  
+  ])
+  };
 
 var apiName = (data) => {
   var queryUrl = `https://api.github.com/users/${data.username}`;
@@ -86,7 +80,36 @@ function writeToFile(fileName, data) {
     }
   }
 
-
+  const createMarkdown = (data) => {
+    console.log(data)
+    return `
+# ${data.projectName}
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+## Description
+  ${data.description}
+ ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+  
+## Installation
+Run these commands to install a dependency:
+    ${data.installation}
+## Usage
+${data.usage}
+## License
+This project is licensed under the ${data.usage} license.
+## Contributing
+${data.contributing}
+## Tests
+Run these commands to run any tests:
+    ${data.tests}
+    `
+  
+};
 // -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
 init();
 
